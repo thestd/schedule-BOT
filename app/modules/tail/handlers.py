dcd0f5a86ac5171231b7b00990bd70dee548d842
skhortiuk <khortiukserhii@ukr.net>
2019-08-30 10:49:09 +0300

    Fixed delete old markups

--- app/modules/tail/handlers.py
+++ app/modules/tail/handlers.py
@@ -9,6 +9,12 @@ async def invalid_msg(message: types.Message, state: FSMContext):
     usr_state = await state.get_state()
     usr_data = await state.get_data()
     if not usr_state or not usr_data:
+        # Delete old markups
+        await bot.send_message(
+            chat_id=message.chat.id,
+            text=".",
+            reply_markup=types.ReplyKeyboardRemove(),
+        )
         await cmd_start(message, state)
         return
 
@@ -19,6 +25,12 @@ async def invalid_clb_data(query: types.CallbackQuery, state: FSMContext):
     usr_state = await state.get_state()
     usr_data = await state.get_data()
     if not usr_state or not usr_data:
+        # Delete old markups
+        await bot.send_message(
+            chat_id=query.message.chat.id,
+            text=".",
+            reply_markup=types.ReplyKeyboardRemove(),
+        )
         await cmd_start(query.message, state)
         return
 
