cc61d30511180b3d594b8b3b5c0affd1a11647a2
skhortiuk <khortiukserhii@ukr.net>
2019-09-24 15:22:12 +0300

    Fixed `BotBlocked` exception

--- app/core/bot.py
+++ app/core/bot.py
@@ -1,16 +1,23 @@
+from typing import Union
+
 from aiogram import Bot, types
 from aiogram.utils.exceptions import (
-    MessageToDeleteNotFound, MessageCantBeDeleted
-)
+    MessageToDeleteNotFound, MessageCantBeDeleted,
+    BotBlocked)
 
 import app.core.misc as misc
 
 
 class ClearBot(Bot):
     async def send_message(self, chat_id, clear=False, *args,
-                           **kwargs) -> types.Message:
-        res = await super(ClearBot, self).send_message(chat_id, *args,
-                                                       **kwargs)
+                           **kwargs) -> Union[types.Message, None]:
+        try:
+            res = await super(ClearBot, self).send_message(chat_id, *args,
+                                                           **kwargs)
+        except BotBlocked:
+            misc.logger.error(f"Bot blocked by: {chat_id}")
+            return
+
         if clear:
             return res
 
