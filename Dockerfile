2dae1789b80878666b6932b6f1f6e3a5cfd61c9d
Serhii Khortiuk <khortiukserhii@ukr.net>
2020-09-28 22:36:53 +0300

    Added sentry.

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,7 @@
 FROM python:3.7.4
 ENV PYTHONUNBUFFERED 1
 RUN mkdir /app
+COPY ./requirements.txt /app
 WORKDIR /app
-COPY ./ ./
-VOLUME /app/logs
 RUN pip install -r requirements.txt
 ENTRYPOINT ["python3", "manage.py", "run"]
--- manage.py
+++ manage.py
@@ -1,8 +1,19 @@
+import os
+
+import sentry_sdk
 import sys
+
+from sentry_sdk.integrations.aiohttp import AioHttpIntegration
 
 if __name__ == '__main__':
     cmd = sys.argv[1]
 
     if cmd == 'run':
+        SENTRY_HOST = os.getenv("SENTRY_HOST")
+        if SENTRY_HOST:
+            sentry_sdk.init(
+                SENTRY_HOST,
+                integrations=[AioHttpIntegration()]
+            )
         from app.main import run
         run()
